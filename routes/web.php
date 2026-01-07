25301268d651281b6d4343d2346f163fe0540f18
nadyadhann <nadya.dhann@gmail.com>
2026-01-07 21:24:59 +0700

    hash pass

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ Route::get('/auth/login', function () {
     return response()->file(public_path('login.html'));
 });
 
+use Illuminate\Support\Facades\Hash;
+
+Route::get('/debug-hash', function () {
+    $hash = 'PASTE_HASH_DARI_DATABASE_DI_SINI';
+    return Hash::check('123', $hash) ? 'HASH OK' : 'HASH GAGAL';
+});
