7308d544846781ccc49c86370a20b5905205c500
nadyadhann <nadya.dhann@gmail.com>
2026-01-07 17:14:12 +0700

    memindahkan htaccess dan indexphp

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Maintenance mode
-if (file_exists($maintenance = __DIR__.'/../laravel_app/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Autoload
-require __DIR__.'/../laravel_app/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 // Bootstrap app
 /** @var Application $app */
-$app = require_once __DIR__.'/../laravel_app/bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
